0db2ee465accfcb77517b508ec7fbd97a249b0b2
gonzalezbri <4717bg@gmail.com>
2023-10-02 23:16:03 -0700

    pages and minor styling

---
+++ panaderia/app/request-a-quote/page.jsx
@@ -0,0 +1,8 @@
+
+export default function requestquote() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>request a quote</h1>
+      </main>
+    )
+  }
