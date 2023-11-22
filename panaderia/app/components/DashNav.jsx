0c2444ba2124fa0baf3693ba205a82bf2c9a2210
gonzalezbri <4717bg@gmail.com>
2023-11-21 23:05:32 -0800

    fixed a spelling error

--- panaderia/app/components/DashNav.jsx
+++ panaderia/app/components/DashNav.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function DashNav () {
     return (
         <nav className="flex justify-between items-center bg-black bg-opacity-80 px-8 py-3">
-            <h1 className="text-white font-bold">Olga's Menu DashBoard made by her favorite son Brian Gonzalez</h1>
+            <h1 className="text-white font-bold">Olga&apos;s Menu DashBoard made by her favorite son Brian</h1>
             <Link className="bg-white p-2" href={'/addBread'}>Add Bread Item</Link>
         </nav>
     )
