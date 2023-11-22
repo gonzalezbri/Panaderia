1ced03d25086f385cf82d307c7be964b0ff5e979
gonzalezbri <4717bg@gmail.com>
2023-11-22 00:43:46 -0800

    some config

--- panaderia/next.config.js
+++ panaderia/next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    experimental: {
+      appDir: true,
+    },
+    async headers() {
+      return [
+        {
+          source: '/api/:path*',
+          headers: [
+            { key: 'Access-Control-Allow-Origin', value: '*' }, // Replace '*' with the specific origin if required
+            { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE' }, // Add allowed methods
+            { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' }, // Add allowed headers
+          ],
+        },
+      ];
+    },
+  };
