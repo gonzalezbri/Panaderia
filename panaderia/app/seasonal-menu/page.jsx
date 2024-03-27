0a28ba5be7a0cebf9f6c05877d2ed90c4334ab06
gonzalezbri <4717bg@gmail.com>
2024-03-26 21:29:12 -0700

    menu styling fixes

--- panaderia/app/seasonal-menu/page.jsx
+++ panaderia/app/seasonal-menu/page.jsx
@@ -49,51 +49,47 @@ export default function CardGrid() {
     }, []);
 
     return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-14">
-        <h1 className="shadow-xl mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
-        Seasonal Menu
-        </h1>
-        <div className="container mx-auto py-12">
-        <div
-            className={`grid ${isMobile ? "grid-cols-1" : "grid-cols-3"} gap-4`}
-        >
-            <AnimatePresence>
-            {breads.map((bread) => (
-                <motion.div
-                key={bread._id}
-                className="card w-full bg-opacity-50 bg-black rounded-l rounded-r overflow-hidden shadow-md"
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -20 }}
-                >
-                <div>
-                    <CldImage  
-                    className="card-image"
-                    src={bread.imageUrl}
-                    alt={bread.title}
-                    height={1}
-                    width={500}
-                    />
+        <div className="flex min-h-screen flex-col items-center justify-between p-8">
+            <h1 className="shadow-xl mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
+                Seasonal Menu
+            </h1>
+            <div className="container mx-auto py-12">
+                <div className={`grid ${isMobile ? "grid-cols-1" : "grid-cols-3"} gap-4`}>
+                    <AnimatePresence>
+                        {breads.map((bread) => (
+                            <motion.div
+                                key={bread._id}
+                                className="card w-full bg-opacity-50 bg-black rounded-l rounded-r overflow-hidden shadow-md"
+                                initial={{ opacity: 0, y: 20 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, y: -20 }}
+                                style={{ height: 'auto' }} // Set height dynamically
+                            >
+                                <div>
+                                    <CldImage
+                                        className="card-image"
+                                        src={bread.imageUrl}
+                                        alt={bread.title}
+                                        height={1}
+                                        width={500}
+                                    />
+                                </div>
+                                <div className="p-2 py-2" style={{ maxHeight: '100%', overflow: 'hidden' }}>
+                                    <p className="text-2xl font-bold text-gray-300 mb-2">
+                                        {bread.title}
+                                    </p>
+                                    <p className="text-l font-bold text-green-400 mb-2">
+                                        ${bread.price}
+                                    </p>
+                                    <p className="font-bold text-gray-400">
+                                        {bread.description}
+                                    </p>
+                                </div>
+                            </motion.div>
+                        ))}
+                    </AnimatePresence>
                 </div>
-                <div className="p-4 py-2">
-                    <p className="text-2xl font-bold text-gray-300 mb-2">
-                    {" "}
-                    {bread.title}
-                    </p>
-                    <p className="text-l font-bold text-green-400 mb-2">
-                    {" "}
-                    ${bread.price}
-                    </p>
-                    <p className="font-bold text-gray-400">
-                    {" "}
-                    {bread.description}
-                    </p>
-                </div>
-                </motion.div>
-            ))}
-            </AnimatePresence>
+            </div>
         </div>
-        </div>
-    </div>
     );
 }
